de1cc149e2f6f65afb08eabe76d1748eae3d03e8
ChesterChowWOV <82011969+ChesterChowWOV@users.noreply.github.com>
2021-06-11 19:48:48 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+function toggleDark() {
+  var body = document.body;
+  body.classList.toggle("bodydark");
+  console.log("Info: Dark Mode toggled");
+}
+function pop(url) {
+  window.open(url)
+}
+console.log("Info: JavaScript is enabled")
